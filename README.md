f71294e5bd37945f622e639c31e039268b55f655
shahzad6077 <alim6077@gmail.com>
2021-10-10 01:44:00 +0500

    :sparkles: added initial files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Muhammad Shahzad Ali - CLI
+
+
+Execute this command in the terminal.
+`npx mshahzadali`
---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+console.log(`
+Muhammad Shahzad Ali
+
+Curious to learning new things. Currently working on Reactjs, Graphql, AWS, and other Serverless Tech.
+
+
+Twitter:    https://twitter.com/shahzad6077
+GitHub:     https://github.com/Shahzad6077
+LinkedIn:   https://www.linkedin.com/in/mshahzadali/
+
+`)
